3a157ceaf1032a4826bb434ebfbad848df483203
Zviad Kvaratskhelia <zviad.kvara000@gmail.com>
2025-02-12 20:38:16 +0400

    Added devide operation

--- calc.py
+++ calc.py
@@ -9,3 +9,6 @@ print(f"Sub is {x - y}")
 
 # the multiple code added by back_end developers
 print(f"Mult is {x * y}")
+
+# the devide operation added by beck_end developers
+print(f"Div is {x // y}")
