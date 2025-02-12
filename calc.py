959f0a52637520a85fa1d5f114d484d37d0bfa6e
Zviad Kvaratskhelia <zviad.kvara000@gmail.com>
2025-02-12 20:27:39 +0400

    Add decorator to calculator

--- calc.py
+++ calc.py
@@ -1,5 +1,11 @@
+# the decoration made by front_end developers
+print("Decorated calculator by front_end developers")
+
 x = int(input("X: "))
 y = int(input("Y: "))
 
 print(f"Sum is {x + y}")
 print(f"Sub is {x - y}")
+
+# the multiple code added by back_end developers
+print(f"Mult is {x * y}")
